dd728079f4c244cd8c2d39342914a0c526be959e
DoTuanBac <43380137+DoTuanBac@users.noreply.github.com>
2020-05-13 17:04:31 +0700

    update

---
+++ main.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+  
+  var days = document.querySelector('.days span');
+  var hour = document.querySelector('.hour');
+  var min = document.querySelector('.min');
+  var second = document.querySelector('.second');
+  var startDate = new Date("May 16, 2020 20:00:00");
+
+  countTime();
+
+  function countTime() {
+    let today = new Date().getTime();
+    let md = (startDate-today);
+    let ms = md  % 86400000;
+    days.innerText = Math.floor(md/(86400000));
+    hour.innerText = Math.floor(ms / 3600000);
+    min.innerText = Math.floor(ms % 3600000 / 60000);
+    second.innerText = Math.floor(ms % 3600000 % 60000 / 1000);
+
+  }
+
+  setInterval(countTime, 1000);
+
+}, false);
+
